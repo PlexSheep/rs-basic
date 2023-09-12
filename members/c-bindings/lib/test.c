27afb6247b356bf0a147b2985de42deb66c883f0
Christoph J. Scherr <Christ.Scherr@newtec.de>
2023-09-12 18:21:59 +0200

    c-binding demo

--- members/c-bindings/lib/test.c
+++ members/c-bindings/lib/test.c
@@ -1,5 +1,17 @@
 #include "test.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int ret19() {
 	return 19;
+}
+
+char* structInfo(MyStruct *st) {
+		char* buf = malloc(1024*sizeof(char));
+		/* snprintf(buf, sizeof(buf), "foo: %d\n", st->foo); */
+		/* snprintf(buf, sizeof(buf), "bar: %c", st->bar); */
+		sprintf(buf,"Infos about the struct:\nfoo:\t%d\nbar:\t%c\n\ngreetings from C", st->foo, st->bar);
+
+		return buf;
 }
--- members/c-bindings/lib/test.h
+++ members/c-bindings/lib/test.h
@@ -1,1 +1,6 @@
 int ret19();
+typedef struct MyStruct {
+	int foo;
+	char bar;
+} MyStruct;
+char* stuctInfo(MyStruct st);
