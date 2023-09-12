7a867a14d205c933213f4b00dd8e959d18dd8045
Christoph J. Scherr <Christ.Scherr@newtec.de>
2023-09-12 17:01:33 +0200

    workspace setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rust basics
+
+This repository aims to contain basics for the rust language.
+
+## Structure
+
+- [`modules`] contains complete small `cargo` projects
+- [`src/bin`] contains binaries that are too small to justify a complete
+    directory.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!(
+"This is the default executable! It does not do much, use another executable.
+
+Select your target like this:
+`cargo run --bin $TARGET`
+
+To see a list of all runnable binaries, you can use the following command.
+`cargo run --bin`
+");
 }
