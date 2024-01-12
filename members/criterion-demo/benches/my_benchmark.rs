c5498b5b123d1c5689df4226c6627e5a9b96c681
Christoph J. Scherr <Christoph.Scherr@newtec.de>
2024-01-12 13:35:37 +0100

    rename criterion-demo and add cucumber-demo

--- members/criterion-demo/benches/my_benchmark.rs
+++ members/criterion-demo/benches/my_benchmark.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use criterion_ex::fibonacci;
+use criterion_demo::fibonacci;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
