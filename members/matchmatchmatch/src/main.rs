17da4f55f48cd2bf314ec4a11d83724906288c9f
Christoph J. Scherr <Christoph.Scherr@newtec.de>
2024-02-15 17:00:41 +0100

    this is literally the stupidest program ever

--- members/matchmatchmatch/src/main.rs
+++ members/matchmatchmatch/src/main.rs
@@ -1,8 +1,273 @@
+/// I did it. I created the stupidest program ever
+///
+/// this is so stupid, it literally makes rustfmt time out.
 fn main() {
     let deep_type = Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
+        Option::Some(Option::Some(Option::Some(Option::Some(Option::Some(
         Option::Some(Option::Some(Option::Some(1337))),
-    )))));
-    match match match match match match match match deep_type {
+    )))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+;
+    match match match match match match match match match match match match match match match match match match
+    match match match match match match match match match match match match match match match match match match
+    match match match match match match match match match match match match match match match match match match
+    match match match match match match match match match match match match match match match match match match
+    match match match match match match match match match match match match match match match match
+        deep_type {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
+        Some(inner) => inner,
+        None => unreachable!(),
+    } {
         Some(inner) => inner,
         None => unreachable!(),
     } {
